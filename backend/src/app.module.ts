369f9538d9c2bf9bc730f686f238e3e9154df0ef
Mohamed-Affes <mohamedaffes991@gmail.com>
2025-07-26 14:40:06 +0100

    uypdate

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ProductModule } from './produit/product.module';
+import { CartModule } from './cart/cart.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ProductModule, CartModule],
 })
 export class AppModule {}
