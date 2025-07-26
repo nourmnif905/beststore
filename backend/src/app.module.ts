874b0cca09d9abc939e0c7ec126b6c7c1594c88b
nourmnif905 <mnifnour24@gmail.com>
2025-07-26 15:28:15 +0100

    update

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ProductModule } from './produit/product.module';
 import { CartModule } from './cart/cart.module';
-
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 @Module({
-  imports: [ProductModule, CartModule],
+  imports: [ProductModule,
+       CartModule
+     ,ConfigModule.forRoot({
+      isGlobal: true, 
+    }),
+    PrismaModule,],
 })
 export class AppModule {}
